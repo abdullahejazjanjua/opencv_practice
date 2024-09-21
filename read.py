125b7418d8e15c9b36b4525cde00f8cf1d0126fe
abdullahejazjanjua <abdullahejaz822@gmail.com>
2024-09-22 00:17:33 +0500

    Added read.py for videos N imgs

--- read.py
+++ read.py
@@ -1,7 +1,20 @@
 import cv2 as cv
 
-img = cv.imread('photos/cat.jpeg')
+img = cv.imread('photos/cat.jpg')
 
 cv.imshow('Cat', img)
 
-cv.waitKey(1)
+
+capture = cv.VideoCapture('videos\\cat.mp4')
+
+while True:
+    isTrue, frame = capture.read()
+    if not isTrue:
+        print("Error: Could not read frame")
+        break
+    elif cv.waitKey(20) & 0xFF == ord('d'):
+        break
+    cv.imshow('Video', frame)
+
+capture.release()
+cv.destroyAllWindows()
